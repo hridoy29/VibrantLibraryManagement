b556447e3d8e38ce8bd0eccd29c3b8f0acb0aaae
jubair <42520040+KhalidRupam@users.noreply.github.com>
2024-12-16 11:55:56 +0600

    Fix login

--- VibrantLibraryManagement.ServiceLayer/Services/LoginService.cs
+++ VibrantLibraryManagement.ServiceLayer/Services/LoginService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,16 @@ namespace VibrantLibraryManagement.ServiceLayer.Services
     public class LoginService : ILoginService
     {
         private readonly HttpClient _httpClient;
+        private readonly IJSRuntime _jSRuntime;
+        private readonly IServiceProvider _serviceProvider;
         private readonly IConfiguration _configuration;
         private readonly string clientId;
 
-        public LoginService(HttpClient httpClient, IConfiguration configuration)
+        public LoginService(HttpClient httpClient, IConfiguration configuration, IJSRuntime jSRuntime,IServiceProvider serviceProvider)
         {
             _httpClient = httpClient;
+            _jSRuntime = jSRuntime;
+            _serviceProvider = serviceProvider;
             clientId = configuration["Api:ClientId"];
         }
 
@@ -27,6 +32,9 @@ namespace VibrantLibraryManagement.ServiceLayer.Services
             {
                 var loginUrl = "/Auth/user/login?clientId=" + clientId;
                 var res = await _httpClient.PostAsJsonAsync(loginUrl, loginEntity);
+
+                var loginResponse = await res.Content.ReadFromJsonAsync<LoginResponse>();
+                StoreToken(loginResponse.AccessToken);
                 return true;
             }
             catch (Exception ex)
@@ -35,5 +43,11 @@ namespace VibrantLibraryManagement.ServiceLayer.Services
                 throw;
             }
         }
-    }
+
+        public void StoreToken(string token)
+        { // Example using local storage in Blazor
+           var jsRuntime = (IJSRuntime)_serviceProvider.GetService(typeof(IJSRuntime)); 
+            jsRuntime.InvokeVoidAsync("localStorage.setItem", "authToken", token);
+
+        }
 }
